509c3b02d8f1dfbd5965a6f0abfaa3d32dc23b8c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-13 13:54:57 +1300

    feat: server setup

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('client-ready', () => {
+    socket.emit('client-ready');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send(`
+  <div style="display:flex; justify-content:center; align-items:center; flex-direction:column; width:100%; height:100%;"
+  <h1 style="text-align:center;">Hello from server</h1>
+  <h2 style="text-align:center;">Send request to begin</h2>
+  <h3 style="text-align:center;"><a href='http://localhost:3000'>Go back to client</a></h3>
+  </div>
+  `);
+});
+
+server.listen(3001, () => {
+  console.log('Server is listening on port 3001');
+});
