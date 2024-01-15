c7012d48d1f6bcf04e638b917a18573174d06743
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-15 18:52:16 +1300

    fix: check win condition

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 
 import { BoardItem, createBoardArray } from './logics/constants';
+import { checkWinner } from './logics/checkWin';
 
 const app = express();
 const server = http.createServer(app);
@@ -12,24 +13,44 @@ const io = new Server(server, {
     origin: '*',
   },
 });
+type RoomDetail = {
+  roomId: string;
+  roomUsers: string[];
+};
+
+let currentRooms = [] as RoomDetail[];
 
 io.on('connection', (socket) => {
   //creating new room
   socket.on('creating-room', ({ roomId }: { roomId: string }) => {
     socket.join(roomId);
     console.log('CREATING ', roomId);
+    //add room to session
+    const newRoom = { roomId, roomUsers: [socket.id] };
+    currentRooms.push(newRoom);
     const newBoard = createBoardArray();
     //successfully created room
     socket.emit('room-created', roomId);
     //send init board
-    socket.emit('init-board', { newBoard });
+    socket.emit('init-board', {
+      newBoard,
+      userInRoom: newRoom.roomUsers.length,
+    });
   });
 
   //new player requesting board
   socket.on('request-board', ({ roomId }: { roomId: string }) => {
     socket.join(roomId);
     console.log('JOINING ', roomId);
-    socket.broadcast.to(roomId).emit('new-player-joined');
+    const currentRoom = currentRooms.find((item) => item.roomId == roomId);
+    console.log(currentRoom, 'BEFORE PUSH');
+    if (currentRoom) {
+      currentRoom.roomUsers.push(socket.id);
+      console.log(currentRoom, 'currentROOM');
+      socket.broadcast.to(roomId).emit('new-player-joined', {
+        users: currentRoom.roomUsers.length,
+      });
+    }
   });
 
   //sending current board to new player
@@ -37,9 +58,64 @@ io.on('connection', (socket) => {
     'current-board',
     ({ board, roomId }: { board: BoardItem[][]; roomId: string }) => {
       console.log('REQUESTING ', roomId);
-      socket.broadcast.to(roomId).emit('your-board', { board });
+      const currentRoom = currentRooms.find((item) => item.roomId == roomId);
+      console.log(currentRoom, 'CURRENT ROOM');
+      if (currentRoom)
+        socket.broadcast.to(roomId).emit('your-board', {
+          board,
+          userInRoom: currentRoom.roomUsers.length,
+        });
     }
   );
+
+  //Finish a turn
+  socket.on(
+    'finished-turn',
+    (data: {
+      currentPlayer: string;
+      newBoard: BoardItem[][];
+      recentMove: number[];
+      roomId: string;
+    }) => {
+      //TODO Checkwin
+      const [x, y] = data.recentMove;
+      console.log(x, y, 'LINE80 Index');
+      const isWinner = checkWinner({ x, y }, data.newBoard);
+      socket.broadcast.emit('next-turn', {
+        turn: data.currentPlayer == 'X' ? 'O' : 'X',
+        currentBoard: data.newBoard,
+      });
+      if (isWinner) {
+        io.emit('game-end', {
+          winner: data.currentPlayer,
+        });
+      }
+    }
+  );
+
+  //clear
+
+  socket.on('clear', (roomId: string) => {
+    io.to(roomId).emit('clear-game');
+  });
+
+  //currentplayers
+  socket.on('get-current-players', (roomId: string) => {
+    console.log(roomId);
+  });
+
+  //disconnecting
+  socket.on('disconnect', () => {
+    currentRooms = currentRooms.map((item) => {
+      if (item.roomUsers.includes(socket.id)) {
+        return {
+          ...item,
+          roomUsers: item.roomUsers.filter((id) => id !== socket.id),
+        };
+      }
+      return item;
+    });
+  });
 });
 
 app.get('/', (_, res) => {
--- logics/checkWin.ts
+++ logics/checkWin.ts
@@ -5,63 +5,88 @@ type Point = {
   y: number;
 };
 
-const checkWin = ({ x, y }: Point, currentBoard: BoardItem[][]) => {
-  let turn = currentBoard[x][y].value;
-  const topLeft = [
-    [x - 3, y - 3],
-    [x - 2, y - 2],
-    [x - 1, y - 1],
-  ];
-  const topRight = [
-    [x + 3, y - 3],
-    [x + 2, y - 2],
-    [x + 1, y - 1],
-  ];
-  const bottomLeft = [
-    [x - 3, y + 3],
-    [x - 2, y + 2],
-    [x - 1, y + 1],
-  ];
-  const bottomRight = [
-    [x + 1, y + 1],
-    [x + 2, y + 2],
-    [x + 3, y + 3],
-  ];
-  const top = [
-    [x, y - 3],
-    [x, y - 2],
-    [x, y - 1],
-  ];
-  const bottom = [
-    [x, y + 1],
-    [x, y + 2],
-    [x, y + 3],
-  ];
-  const left = [
+export const checkWinner = ({ x, y }: Point, currentBoard: BoardItem[][]) => {
+  const turn = currentBoard[x][y].value;
+  console.log(turn, 'LINE 10');
+  console.log('CURRENTBOARD', currentBoard);
+  const horizontal = [
     [x - 3, y],
     [x - 2, y],
     [x - 1, y],
-  ];
-  const right = [
+    [x, y],
     [x + 1, y],
     [x + 2, y],
     [x + 3, y],
   ];
 
-  const checkLineWin = (line: number[][]) => {
-    return line.every(([x, y]) => currentBoard[x][y].value == turn);
-  };
-
-  const winningLines = [
-    topLeft,
-    top,
-    topRight,
-    left,
-    right,
-    bottomLeft,
-    bottom,
-    bottomRight,
+  const vertical = [
+    [x, y - 3],
+    [x, y - 2],
+    [x, y - 1],
+    [x, y],
+    [x, y + 1],
+    [x, y + 2],
+    [x, y + 3],
   ];
 
-  return winningLines.some((line) => checkLineWin(line));
+  const diag1 = [
+    [x - 3, y - 3],
+    [x - 2, y - 2],
+    [x - 1, y - 1],
+    [x, y],
+    [x + 1, y + 1],
+    [x + 2, y + 2],
+    [x + 3, y + 3],
+  ];
+
+  const diag2 = [
+    [x + 3, y - 3],
+    [x + 2, y - 2],
+    [x + 1, y - 1],
+    [x, y],
+    [x - 1, y + 1],
+    [x - 2, y + 2],
+    [x - 3, y + 3],
+  ];
+
+  const slidingWindow = (line: number[][]) => {
+    const points = line.map((numArr) => {
+      const [x, y] = numArr;
+      if (
+        x < 0 ||
+        y < 0 ||
+        x > currentBoard.length - 1 ||
+        y > currentBoard[0].length - 1
+      ) {
+        return 0;
+      }
+      const current = currentBoard[x][y].value;
+      return current == turn ? 1 : 0;
+    });
+
+    let max = 0;
+    for (let i = 0; i < 4; i++) {
+      max += points[i];
+    }
+
+    if (max === 4) return true;
+
+    for (let i = 4; i < line.length - 1; i++) {
+      max = max - points[i - 4] + points[i];
+      if (max === 4) return true;
+    }
+
+    return false;
+  };
+
+  const horizontalWin = slidingWindow(horizontal);
+  console.log('HOR');
+  const verticalWin = slidingWindow(vertical);
+  console.log('ver');
+  const diag1Win = slidingWindow(diag1);
+  console.log('diag1');
+  const diag2Win = slidingWindow(diag2);
+  console.log('diag2');
+
+  return horizontalWin || verticalWin || diag1Win || diag2Win;
 };
